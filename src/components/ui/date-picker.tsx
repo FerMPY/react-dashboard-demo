0f833c1c4d124aba516926aa6ff8b9c6d1789aa1
Fernando <31524850+FerMPY@users.noreply.github.com>
2024-12-22 02:44:38 -0300

    new components

---
+++ src/components/ui/date-picker.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+
+type DatePickerProps = {
+  value?: Date;
+  onChange: (date: Date | undefined) => void;
+};
+
+export function DatePicker({ value, onChange }: DatePickerProps) {
+  const currentYear = new Date().getFullYear();
+  const startYear = currentYear - 120;
+  const endYear = currentYear;
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[240px] justify-start text-left font-normal",
+            !value && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {value ? format(value, "PPP") : <span>Pick a date</span>}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent align="start" className="w-auto p-0">
+        <Calendar
+          mode="single"
+          captionLayout="dropdown-buttons"
+          selected={value}
+          onSelect={onChange}
+          fromYear={startYear}
+          toYear={endYear}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+}
