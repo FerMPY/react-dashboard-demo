b2aaeb5737108732c673ba037cc3368e333fac6a
Fernando <31524850+FerMPY@users.noreply.github.com>
2024-12-31 03:30:30 -0300

    working json-server for mocking api calls

--- generateDb.ts
+++ generateDb.ts
@@ -5,34 +5,41 @@ import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-// Generate mock users
 const generateUsers = (count: number) => {
-  return Array.from({ length: count }, () => ({
-    id: faker.string.ulid(),
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-    avatar: faker.image.avatar(),
-    creditCards: Array.from(
-      { length: faker.number.int({ min: 1, max: 3 }) },
-      () => ({
-        cardNumber: faker.finance.creditCardNumber(),
-        cardType: "Master Card",
-        expiryDate: faker.date.future().toISOString().split("T")[0],
-        balance: parseFloat(
-          faker.finance.amount({ min: 100, max: 10000, dec: 2 }),
-        ),
-      }),
-    ),
-  }));
+  return Array.from({ length: count }, () => {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+
+    return {
+      id: faker.string.ulid(),
+      name: `${firstName} ${lastName}`,
+      email: faker.internet.email({ firstName, lastName }),
+      username: faker.internet.username({ firstName, lastName }),
+      avatar: faker.image.avatar(),
+      password: faker.internet.password({ length: 6 }),
+      creditCards: Array.from(
+        { length: faker.number.int({ min: 1, max: 3 }) },
+        () => ({
+          cardNumber: faker.finance.creditCardNumber("mastercard"),
+          cardType: "Master Card",
+          cardHolder: `${firstName} ${lastName}`,
+          expiryDate: faker.date.future().toISOString().split("T")[0],
+          balance: parseFloat(
+            faker.finance.amount({ min: 100, max: 10000, dec: 0 }),
+          ),
+        }),
+      ),
+    };
+  });
 };
 
-// Generate the mock database
 const db = {
   users: generateUsers(6),
 };
 
-// Write the data to `public/db.json`
-const filePath = resolve(__dirname, "public", "db.json");
+export type JsonDB = typeof db;
+
+const filePath = resolve(__dirname, "db.json");
 writeFileSync(filePath, JSON.stringify(db, null, 2));
 
 console.log(`Mock database generated at ${filePath}`);
