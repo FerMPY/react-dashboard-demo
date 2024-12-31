34cd4fc7edb5bcc88ebf11ea5cd214220e808eb4
Fernando <31524850+FerMPY@users.noreply.github.com>
2024-12-31 03:58:43 -0300

    more vercel issues

--- api/index.js
+++ api/index.js
@@ -7,6 +7,6 @@ const router = jsonServer.router(path.join(process.cwd(), "db.json"));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
+server.use("/api", router);
 
 export default server;
